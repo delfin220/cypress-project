28c397d0c71b915609aa98bdb726d8ee77eee003
andriy <andriy.t@kyla.com>
2024-08-11 18:15:26 +0300

    Minor updates

--- cypress/e2e/visit-demoqa.cy.js
+++ cypress/e2e/visit-demoqa.cy.js
@@ -1,9 +1,4 @@
-
 import '../support/commands'
-
-
-
-
 
 describe('Visit demoqa.com-textbox ', () => {
     it('Visit the demoqa and check the main elements is visible', () => {
@@ -13,9 +8,6 @@ describe('Visit demoqa.com-textbox ', () => {
       //Check headers logo
       cy.get('h1').contains('Text Box').should('be.visible')
       cy.get('header a img[src="/images/Toolsqa.jpg"]').should('be.visible') 
-
-
-      
 
       // Checking the left menu items
       cy.get('.header-wrapper').contains('Elements').should('be.visible')
